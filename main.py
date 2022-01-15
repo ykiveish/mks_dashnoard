3e5f83dec126b7a474602d50930e4f2f81358157
Kiveisha <yevgeniy.kiveisha@hp.com>
2022-01-15 10:58:59 +0200

    template fix init
    Signed-off-by: Kiveisha <yevgeniy.kiveisha@hp.com>

--- main.py
+++ main.py
@@ -4,38 +4,23 @@ import argparse
 
 from classes import terminal
 from classes import application
-from classes import nrf
 
 def signal_handler(signal, frame):
 	pass
-
-class Node():
-	def __init__(self):
-		pass
 	
 def main():
 	signal.signal(signal.SIGINT, signal_handler)
 
-	parser = argparse.ArgumentParser(description='Execution module called Node \n Example: python.exe main.py --serial COM31')
+	parser = argparse.ArgumentParser(description='Execution module\n Example: python.exe main.py')
 	parser.add_argument('-v', '--version', action='store_true',
 			help='Version')
-	parser.add_argument('-cn', '--serial', action='store',
-			dest='serial', help='Serial COM to connect')
 	parser.add_argument('-verb', '--verbose', action='store_true',
 			help='Print messages')
 	
 	args = parser.parse_args()
 
-	cli 		= terminal.Terminal()
-	app 		= application.Application()
-	gateway 	= nrf.NRF()
-
-	app.RegisterHardware(gateway)
-	cli.RegisterHardware(gateway)
-	cli.RegisterApplication(app)
-
-	# gateway.RegisterListener(cli.AsyncDataArrived)
-	gateway.RegisterListener(app.AsyncDataArrived)
+	cli = terminal.Terminal()
+	app = application.Application()
 
 	app.Run()
 	cli.Run() # Blocking
--- static/js/resource/pc/app.js
+++ static/js/resource/pc/app.js
@@ -40,10 +40,7 @@ Application.prototype.Connect = function(ip, port, callback) {
         console.log("Connected to local websocket");
 
         // Module area
-        self.API.AppendModule("ModuleDashboardView");
-        self.API.AppendModule("ModuleGatewayView");
-        self.API.AppendModule("ModuleNodeView");
-        self.API.AppendModule("ModuleSensorView");
+        self.API.AppendModule("TemplateModuleView");
         self.API.GetModules();
 
         callback();
@@ -51,12 +48,10 @@ Application.prototype.Connect = function(ip, port, callback) {
 }
 Application.prototype.NodeLoaded = function () {
     console.log("Modules Loaded");
-
-    window.ApplicationModules.DashboardView = new ModuleDashboardView();
+    window.ApplicationModules.DashboardView = new TemplateModuleView();
     window.ApplicationModules.DashboardView.SetHostingID("id_application_view_module");
-    window.ApplicationModules.DashboardView.SetObjectDOMName("window.ApplicationModules.DashboardView");
-    window.ApplicationModules.DashboardView.Build(null, function(module) {});	
-
+    window.ApplicationModules.DashboardView.SetObjectDOMName("window.ApplicationModules.TemplateModuleView");
+    window.ApplicationModules.DashboardView.Build(null, function(module) {});
 }
 Application.prototype.OnChangeEvent = function(packet) {
     var event = packet.payload.event;
@@ -64,129 +59,13 @@ Application.prototype.OnChangeEvent = function(packet) {
     this.Publish(event, data);
 }
 // ASYNC REGISTERED HANDLERS
-Application.prototype.SystemInfoHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SerialListHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetNodeListHandler = function(data, scope) {
-    if (data === undefined || data === null) {
-        return;
-    }
-
-    window.ApplicationModules.DashboardView.GetDevicesEvent(data.list);
-}
-Application.prototype.GetRemoteNodeInfoHandler = function(data, scope) {
-    window.ApplicationModules.DashboardView.NRFDataEvent(data);
-}
-Application.prototype.GetRemoteNodeDataHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SetRemoteNodeDataHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetDeviceTypeHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetDeviceAdditionalHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SetNodeAddressHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetNodeAddressHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetNodeInfoHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GetNodesMapHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.AddNodeIndexHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.DelNodeIndexHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SetRemoteNodeAddressHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.DelNodeIndexHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.USBDeviceConnectedHandler = function(data, scope) {
-    window.ApplicationModules.DashboardView.USBDeviceConnectedEvent(data);
-}
-Application.prototype.USBDeviceDisconnectedHandler = function(data, scope) {
-    window.ApplicationModules.DashboardView.USBDeviceDisconnectedEvent(data);
-}
-Application.prototype.SetWorkingPortHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.GatewayInfoHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.NRFPacket = function(data, scope) {
-    window.ApplicationModules.DashboardView.NRFDataEvent(data);
-}
-Application.prototype.InsertDeviceHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SelectSensorsHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.DeleteDeviceHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SelectDevicesHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.UpdateSensorInfoHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.DeviceCommunicationLostHandler = function(data, scope) {
-    window.ApplicationModules.DashboardView.DeviceConnectionLostEvent(data);
-}
-Application.prototype.SelectSensorsByDeviceHandler = function(data, scope) {
-    console.log(data);
-}
-Application.prototype.SelectSensorHistoryHandler = function(data, scope) {
+Application.prototype.UndefinedHandler = function(data, scope) {
     console.log(data);
 }
 
 var app = new Application();
-app.RegisterEventHandler("SelectSensorHistoryHandler",      app.SelectSensorHistoryHandler,     app);
-app.RegisterEventHandler("SelectSensorsByDeviceHandler",    app.SelectSensorsByDeviceHandler,   app);
-app.RegisterEventHandler("DeviceCommunicationLostHandler",  app.DeviceCommunicationLostHandler, app);
-app.RegisterEventHandler("UpdateSensorInfoHandler",         app.UpdateSensorInfoHandler,        app);
-app.RegisterEventHandler("SelectDevicesHandler",            app.SelectDevicesHandler,           app);
-app.RegisterEventHandler("DeleteDeviceHandler",             app.DeleteDeviceHandler,            app);
-app.RegisterEventHandler("SelectSensorsHandler",            app.SelectSensorsHandler,           app);
-app.RegisterEventHandler("InsertDeviceHandler",             app.InsertDeviceHandler,            app);
-app.RegisterEventHandler("NRFPacket",                       app.NRFPacket,                      app);
-app.RegisterEventHandler("SystemInfoHandler",               app.SystemInfoHandler,              app);
-app.RegisterEventHandler("GatewayInfoHandler",              app.GatewayInfoHandler,             app);
-app.RegisterEventHandler("SerialListHandler",               app.SerialListHandler,              app);
-app.RegisterEventHandler("SetWorkingPortHandler",           app.SetWorkingPortHandler,          app);
-app.RegisterEventHandler("GetNodeListHandler",              app.GetNodeListHandler,             app);
-app.RegisterEventHandler("GetRemoteNodeInfoHandler",        app.GetRemoteNodeInfoHandler,       app);
-app.RegisterEventHandler("GetRemoteNodeDataHandler",        app.GetRemoteNodeDataHandler,       app);
-app.RegisterEventHandler("SetRemoteNodeDataHandler",        app.SetRemoteNodeDataHandler,       app);
-app.RegisterEventHandler("GetDeviceTypeHandler",            app.GetDeviceTypeHandler,           app);
-app.RegisterEventHandler("GetDeviceAdditionalHandler",      app.GetDeviceAdditionalHandler,     app);
-app.RegisterEventHandler("SetNodeAddressHandler",           app.SetNodeAddressHandler,          app);
-app.RegisterEventHandler("GetNodeAddressHandler",           app.GetNodeAddressHandler,          app);
-app.RegisterEventHandler("GetNodeInfoHandler",              app.GetNodeInfoHandler,             app);
-app.RegisterEventHandler("GetNodesMapHandler",              app.GetNodesMapHandler,             app);
-app.RegisterEventHandler("AddNodeIndexHandler",             app.AddNodeIndexHandler,            app);
-app.RegisterEventHandler("DelNodeIndexHandler",             app.DelNodeIndexHandler,            app);
-app.RegisterEventHandler("SetRemoteNodeAddressHandler",     app.SetRemoteNodeAddressHandler,    app);
-app.RegisterEventHandler("GetRemoteNodeAddressHandler",     app.GetRemoteNodeAddressHandler,    app);
-app.RegisterEventHandler("USBDeviceConnectedHandler",       app.USBDeviceConnectedHandler,      app);
-app.RegisterEventHandler("USBDeviceDisconnectedHandler",    app.USBDeviceDisconnectedHandler,   app);
 
+app.RegisterEventHandler("undefined", app.UndefinedHandler, app);
 app.Connect(global_ip, global_port, function() {});
 
 feather.replace();
